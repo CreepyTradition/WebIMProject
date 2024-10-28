83f8aeb4ac8dd79458a8d1668225553d9c4d885a
Perkin Abrantes <perkinabrantes@gmail.com>
2024-10-28 22:04:28 +0800

    size test

--- src/components/ui/cards.tsx
+++ src/components/ui/cards.tsx
@@ -7,7 +7,7 @@ interface CardProps {
   
   export default function Card({image, title, children, onClick}: CardProps) {
     return (
-      <div onClick={ onClick } className="flex flex-col gap-[1rem] hover:scale-105 justify-start items-center text-center p-[2rem] rounded-lg border-[1px] border-border bg-[#fff] shadow-md w-[350px] h-auto">
+      <div onClick={ onClick } className="flex flex-col gap-[1rem] hover:scale-105 justify-start items-center text-center p-[2rem] rounded-lg border-[1px] border-border bg-[#fff] shadow-md w-[750px] h-auto">
         {
           image && (
             <img src={image} alt={title} className="w-full h-auto" />
