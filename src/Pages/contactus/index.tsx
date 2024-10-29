4bc2b06663ba5fdb82279b5e9d3dc2fb6d4af660
Perkin Abrantes <perkinabrantes@gmail.com>
2024-10-29 09:38:49 +0800

    added a contact card

--- src/Pages/contactus/index.tsx
+++ src/Pages/contactus/index.tsx
@@ -28,10 +28,13 @@ export default function ContactUs() {
         <section className="flex-col gap-[4rem] h-full">
           <div className="flex flex-col justify-center items-center gap-[2rem]">
             <div className="flex justify-center items-center gap-[1rem] md:gap-[2rem]">
-              <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-[2rem]">
+              <div className="grid grid-cols-1 grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 gap-[2rem]">
                 <Card title="Email" icon="material-symbols:mail-outline" iconSize="text-[10rem]" href="mailto:csa@mcm.edu.ph">csa@mcm.edu.ph</Card>
                 <Card title="Facebook" icon="icon-park:facebook-one" iconSize="text-[8.5rem]" href="https://www.facebook.com/profile.php?id=100070921430794">
                   MCM Center for Student Advising</Card>
+                <Card title="Teams" icon="ph:microsoft-teams-logo-light" iconSize="text-[8.5rem]" href="https://teams.microsoft.com/l/chat/0/0?users=sgdespino@mcm.edu.ph">
+                  Samantha Espino<br />CSA Facilitator</Card>
+                  
               </div>
             </div>
           </div>
